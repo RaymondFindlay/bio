302df08ffc1b0484dd788e6e443d04e552cfad3b
RaymondFindlay <raymond.findlay@gmail.com>
2019-04-22 23:15:21 +0100

    Updates

--- precache-manifest.5497f8600f553bfcc1debd6e070bd682.js
+++ precache-manifest.5497f8600f553bfcc1debd6e070bd682.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/bio/static/js/runtime~main.e11ffc24.js"
   },
   {
-    "revision": "113517f61094b32a3327",
+    "revision": "4e549340d4361e901ebe",
     "url": "/bio/static/js/main.a62d0f63.chunk.js"
   },
   {
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/bio/static/js/2.b151aad8.chunk.js"
   },
   {
-    "revision": "113517f61094b32a3327",
-    "url": "/bio/static/css/main.40e5fc77.chunk.css"
+    "revision": "4e549340d4361e901ebe",
+    "url": "/bio/static/css/main.8d568c3c.chunk.css"
   },
   {
     "revision": "57eeb8aad99b3d1e22bc",
     "url": "/bio/static/css/2.6768516f.chunk.css"
   },
   {
-    "revision": "8a01bc04e55dbd7e04442c880fb48988",
+    "revision": "0168d25904cc71950b62c679b518e4d6",
     "url": "/bio/index.html"
   }
 ];
